bb1ae515c59473ac5febb4a05a5ec3ca43573763
Khôi Nguyễn <khoinm0603@gmail.com>
2025-05-23 15:00:30 +0700

    Add Shift Controller,Update getLessonDate

---
+++ Server/EVOLEC/EVOLEC_Server/Controllers/ShiftController.cs
@@ -0,0 +1,27 @@
+﻿using EVOLEC_Server.Dtos;
+using EVOLEC_Server.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EVOLEC_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShiftController : ControllerBase
+    {
+        [HttpGet("getAllShifts")]
+        public IActionResult getAllShifts()
+        {
+            return Ok(new ResponseEntity<object>
+            {
+
+                Status = true,
+                ResponseCode = 200,
+                StatusMessage = "Successfully",
+                Data = ShiftSchedule._shiftList
+
+            });
+        }
+    }
+}
